d73991e6cbe78d8dc647ba83d7deef104605a809
Rodrigo IQ <riq.dsing@gmail.com>
2022-05-29 22:47:38 -0500

    added triangle perimeter and sides

--- figuras.js
+++ figuras.js
@@ -10,6 +10,17 @@ console.log(`squer Perimenter =  ${squerePerimeter} cm`);
 
 const squareArea = squarelength*squarelength;
 
-console.log(`square area =${squareArea} cm`)
+console.log(`square area =${squareArea} cm`);
 
 
+// code for triangle 
+
+const triangle_side1 = 6;
+const triangle_side2 = 6;
+const base = 4;
+
+const triangle_perimeter  = triangle_side1+triangle_side2+base;
+
+console.log(`Sides of the triangle measure = ${triangle_side1} cm, `
++`${triangle_side2} cm, `
++`${base} cm, `)
