e9dc834fe8b6e2ad9a2c5bb581b9171e9e0719d7
Rodrigo IQ <riq.dsing@gmail.com>
2022-05-29 22:50:17 -0500

    grouped console logs

--- figuras.js
+++ figuras.js
@@ -1,5 +1,5 @@
 // code for square lenght, area, perimeter
-
+console.group("square")
 const squarelength = 5;
 
 console.log(`square leght = ${squarelength} cm`);
@@ -11,10 +11,10 @@ console.log(`squer Perimenter =  ${squerePerimeter} cm`);
 const squareArea = squarelength*squarelength;
 
 console.log(`square area =${squareArea} cm`);
-
+console.groupEnd()
 
 // code for triangle 
-
+console.group("Triangle")
 const triangle_side1 = 6;
 const triangle_side2 = 6;
 const base = 4;
@@ -24,3 +24,4 @@ const triangle_perimeter  = triangle_side1+triangle_side2+base;
 console.log(`Sides of the triangle measure = ${triangle_side1} cm, `
 +`${triangle_side2} cm, `
 +`${base} cm, `)
+console.groupEnd()
