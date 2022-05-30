35ab8a04aee648e6d474290017a2769c06147324
Rodrigo IQ <riq.dsing@gmail.com>
2022-05-29 22:33:23 -0500

    Hard coded square perimeter and area console log

--- figuras.js
+++ figuras.js
@@ -1,11 +1,15 @@
+// code for square lenght, area, perimeter
+
 const squarelength = 5;
 
-console.log(`square leght = ${squarelength}`);
+console.log(`square leght = ${squarelength} cm`);
 
 const squerePerimeter = squarelength*4;
 
-console.log(`squer Perimenter =  ${squerePerimeter}`);
+console.log(`squer Perimenter =  ${squerePerimeter} cm`);
 
 const squareArea = squarelength*squarelength;
 
-console.log(`square area =${squareArea}`)
+console.log(`square area =${squareArea} cm`)
+
+
