afd8c02d8c72912d240f56c5963370370cfd9518
RodrigoIQ <68536874+RodrigoIQ@users.noreply.github.com>
2022-05-30 13:09:11 -0500

    added triangle height measurement and console log

--- figuras.js
+++ figuras.js
@@ -11,17 +11,21 @@ console.log(`squer Perimenter =  ${squerePerimeter} cm`);
 const squareArea = squarelength*squarelength;
 
 console.log(`square area =${squareArea} cm`);
-console.groupEnd()
+console.groupEnd();
 
 // code for triangle 
 console.group("Triangle")
 const triangle_side1 = 6;
 const triangle_side2 = 6;
-const base = 4;
+const triangle_base = 4;
+const triangle_height = 5.5;
 
-const triangle_perimeter  = triangle_side1+triangle_side2+base;
+
+const triangle_perimeter  = triangle_side1+triangle_side2+triangle_base;
 
 console.log(`Sides of the triangle measure = ${triangle_side1} cm, `
 +`${triangle_side2} cm, `
-+`${base} cm, `)
-console.groupEnd()
++`${base} cm, `);
+
+console.log(`triangle height = ${triangle_height} cm`);
+console.groupEnd();
