65bba7d5bd0317ea3f8fe4bd9724c9bb3de7d809
RodrigoIQ <68536874+RodrigoIQ@users.noreply.github.com>
2022-06-01 10:01:19 -0500

    erased console logs not used

--- figuras.js
+++ figuras.js
@@ -1,28 +1,19 @@
 // code for square lenght, area, perimeter
 console.group("square")
-// const squarelength = 5;
-
-// console.log(`square leght = ${squarelength} cm`);
 
 function squarePerimeter(side){
     return side*4
 };
 
-// console.log(`squer Perimenter =  ${squerePerimeter} cm`);
-
 function squareArea(side){
     return side*side
 };
 
-//console.log(`square area =${squareArea} cm`);
 console.groupEnd();
 
 //code for triangle 
  console.group("Triangle")
-// const triangle_side1 = 6;
-// const triangle_side2 = 6;
-// const triangle_base = 4;
-// const triangle_height = 5.5;
+
 
 function triangle_perimeter(side1,side2,base){
     return side1+side2+base
@@ -32,25 +23,11 @@ function triangle_area(height,base){
     return (height*base)/2
 };
 
-
-
-// console.log(`Sides of the triangle measure = ${triangle_side1} cm, `
-// +`${triangle_side2} cm, `
-// +`${triangle_base} cm, `);
-
-// console.log(`triangle height = ${triangle_height} cm`);
-
-// console.log(`trigle perimenter = ${triangle_perimeter} cm`);
-
-// console.log(`triangle Area = ${triangle_area}`);
-
-
 console.groupEnd();
 
-//Circle 
+
 console.group("Circle");
-// const radius = 4;
-// const diameter = radius*2;
+
 const PI = Math.PI;
 
 function diameterCircle(radius){
@@ -67,13 +44,12 @@ function circle_area(radius){
 };
 
 
-//Circle consle logs 
-// console.log(`the radius of the circle is = ${radius}`);
-// console.log(`the diameter of the circle is = ${diameter}`);
-
-// console.log(`Circle circumference = ${circumference}`);
-// console.log(`Circle area = ${circle_area}`);
-
-
 
 console.groupEnd();
+
+
+
+//Interact with html
+
+
+
