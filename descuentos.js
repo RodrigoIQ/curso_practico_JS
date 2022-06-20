599c0a1abbdb54d268d80a2384585e6a6ba46d05
RodrigoIQ <68536874+RodrigoIQ@users.noreply.github.com>
2022-06-20 14:25:49 -0500

    rm typeo

--- descuentos.js
+++ descuentos.js
@@ -9,7 +9,7 @@ function get_Discounted_price(){
         //product_price, discount_percentage
         const product_price = document.getElementById("product_price").value;
         const discount_percentage =  document.getElementById("discount_percentage").value;
-        const resul_Paragraf = document.getElementById("ResultPrice");
+        const result_Paragraf = document.getElementById("ResultPrice");
         const discount=discount_price(product_price,discount_percentage);
         resul_Paragraf.innerText = `Product price is ${product_price} with discount it is ${discount}`
-};s
+};
