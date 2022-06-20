b0fcc9a874449f8aba136e2cb4dff9b1c7dd757d
RodrigoIQ <68536874+RodrigoIQ@users.noreply.github.com>
2022-06-20 14:14:00 -0500

    add discount price with innertText

--- descuentos.js
+++ descuentos.js
@@ -1,8 +1,15 @@
 //In this file I will follow the second project in Platzi practical javascript course 
 
-function Discounted_price(){
+function discount_price(price,percentege){
+        return price*(1-(percentege/100));
+}
+
+
+function get_Discounted_price(){
         //product_price, discount_percentage
         const product_price = document.getElementById("product_price").value;
         const discount_percentage =  document.getElementById("discount_percentage").value;
-        console.log(product_price * discount_percentage);
+        const resul_Paragraf = document.getElementById("ResultPrice");
+        const discount_price=discount_price(product_price,discount_percentage);
+        resul_Paragraf.innerText = `Product price is ${product_price} with discount it is ${discount_price}`
 };
