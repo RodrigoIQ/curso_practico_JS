c0804fc78752d0bb0602c6ea3ffd9b99d5e999e5
RodrigoIQ <68536874+RodrigoIQ@users.noreply.github.com>
2022-07-18 22:56:20 -0500

    update averge docs

---
+++ averge.js
@@ -0,0 +1,18 @@
+// calculate the average of a array 
+// how to make an array from user inpur 
+
+// hard coded array for 
+listOfNumbers = [15,3,2]
+
+// function for average 
+function calculate_average(ListOfNumbers){
+    const sum_listOfNumbers = listOfNumbers.reduce(
+        function (sum_value = 0, new_value){
+            return sum_value + new_value;
+        }
+    );
+
+    const average = sum_listOfNumbers/listOfNumbers;
+    return average
+
+}
