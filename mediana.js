2fac3a46d39decd035ed0c705dde5c330bcf7f4e
RodrigoIQ <68536874+RodrigoIQ@users.noreply.github.com>
2022-07-24 16:12:57 -0500

    add median documents and start code

---
+++ mediana.js
@@ -0,0 +1,18 @@
+// this file is to calcuale the median of a list of numbers 
+
+const listOFnumbersMedian = [1,2,3,4,5]
+
+// calculating a median has 2 options if the list is even or odd 
+// sort list -- future step 
+
+const listLenght = listOFnumbersMedian.length;
+console.log(`list has ${listLenght} items`)
+
+
+// if (list is odd){
+
+//     // get the number in the midel 
+    
+// } else if (list is even){
+//     // calulate the averege of the 2 numbes midel numbers 
+// } ;
